6033c734911c0bb13caac5dfcaef4a4b50f217f6
hk2257853 <hk2257853@gmail.com>
2021-10-09 16:17:30 +0530

    added gitignore

--- Oops/oops lab 2/friend.cpp
+++ Oops/oops lab 2/friend.cpp
@@ -14,14 +14,17 @@ public:
 	//declaring these as friend so that they can access meters variable of this class.
 	friend void addfive(Distance &d);
 	friend void addten(Distance &d);
+
 	//making whole class friend so that all of its functions can access meters variable of this class.
 	friend class addfif;
-	//I don't need to put scope resolution and all?
 };
 
-class add1
+//Note: pass by argument has been
+//done because I want to change value of meter(see the 2nd use of call by ref in notes).
+
+class add1 //a particular function is friend.
 {
-public: //a particular function is friend.
+public:
 	friend void addten(Distance &d)
 	{
 		d.meters = d.meters + 10;
@@ -32,7 +35,7 @@ public: //a particular function is friend.
 class addfif
 {
 public:
-	void addfifteen(Distance &d)
+	void addfifteen(Distance &d) //don't need to mention friend here.
 	{
 		d.meters = d.meters + 15;
 	}
@@ -51,8 +54,9 @@ int main()
 
 	addfive(d1); //non class function
 	d1.DisplayData();
+
 	/*
-	add x;
+	Distance x;
 	x.addfive(d1); //non class function
 	 error: 'class add' has no member named 'addfive'
 	 why? making friend doesn't mean its a part(scope is better term to use) of that class.
